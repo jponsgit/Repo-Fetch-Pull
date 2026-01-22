3be0bc4f5ad24de08216f1e22acce128c6cbad0f
Joan Pons Carreras <jpons21196@iesjoanramis.org>
2026-01-22 16:25:35 +0100

    Última versión del proyecto

--- src/repofetchpull/App.java
+++ src/repofetchpull/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Mensaje modificado desde GitHub");
-
+		System.out.println("Mensaje modificado desde Local (última version)");
 	}
 
 }
